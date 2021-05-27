f7e73517f8ea7c540c74bc81355e8be2092eeeb6
Tim <102419310@student.swin.edu.au>
2021-05-28 02:32:14 +1000

    Notification while app is running

--- MyRecipe/ViewController.swift
+++ MyRecipe/ViewController.swift
@@ -17,7 +17,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         table.register(RecipeListTableViewCell.nib(), forCellReuseIdentifier: RecipeListTableViewCell.identifier)
         table.rowHeight = UITableView.automaticDimension
         // Do any additional setup after loading the view.
-        
+        //show badge
+        //UIApplication.shared.applicationIconBadgeNumber = 0
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 25
+    }
+    
+    /*Deprecated
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // Update the app interface directly.
+        completionHandler(UNNotificationPresentationOptions.banner)
+    }
+    */
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         //1. Create notification center object
         let center = UNUserNotificationCenter.current()
         //2. Ask for permission
@@ -26,31 +43,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         //3. Create notification content, with title and body
         let content = UNMutableNotificationContent()
-        content.title = "My Recipe"
+       // content.setValue("YES", forKeyPath: "shouldAlwaysAlertWhileAppIsForeground")
+        content.title = "Hey there"
         content.body = "Check out for popular recipe!⬆️"
+        content.badge = UIApplication.shared.applicationIconBadgeNumber + 1 as NSNumber;
         content.sound = UNNotificationSound.default
-
-        
         //4. Create a notification trigger
-        let date = Date().addingTimeInterval(5)
+        let date = Date().addingTimeInterval(1)
         let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-        
         //5. Create a request to encapsule items above as an object
         let uuidString = UUID().uuidString
         let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
-        
         //6. Register the request with notificatino center
         center.add(request){(error) in
-            //error parameter
+            if error != nil {
+                  // Handle any errors.
+               }
         }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 25
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) ->CGFloat{
