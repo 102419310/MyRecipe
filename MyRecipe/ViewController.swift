dfc1e849d3301125639582016a02552fddc2207e
Tim <102419310@student.swin.edu.au>
2021-05-27 17:49:12 +1000

    Sample notification push

--- MyRecipe/ViewController.swift
+++ MyRecipe/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -16,10 +17,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         table.register(RecipeListTableViewCell.nib(), forCellReuseIdentifier: RecipeListTableViewCell.identifier)
         table.rowHeight = UITableView.automaticDimension
         // Do any additional setup after loading the view.
+        
+        //1. Create notification center object
+        let center = UNUserNotificationCenter.current()
+        //2. Ask for permission
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            //do something if denied
+        }
+        //3. Create notification content, with title and body
+        let content = UNMutableNotificationContent()
+        content.title = "My Recipe"
+        content.body = "Check out for popular recipe!⬆️"
+        content.sound = UNNotificationSound.default
+
+        
+        //4. Create a notification trigger
+        let date = Date().addingTimeInterval(5)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        //5. Create a request to encapsule items above as an object
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        //6. Register the request with notificatino center
+        center.add(request){(error) in
+            //error parameter
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 25
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
