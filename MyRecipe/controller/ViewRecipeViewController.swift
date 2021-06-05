a65dea21b70d5fc60a8092841c0afb42d1fa2d61
Tim <102419310@student.swin.edu.au>
2021-06-05 17:41:57 +1000

    share on twitter/bug fix on view

--- MyRecipe/controller/ViewRecipeViewController.swift
+++ MyRecipe/controller/ViewRecipeViewController.swift
@@ -15,6 +15,30 @@ class ViewRecipeViewController: UIViewController {
     @IBOutlet weak var ingredients: UILabel!
     @IBOutlet weak var steps: UILabel!
     
+    @IBAction func share(_ sender: Any) {
+        //content of the tweet
+        let tweetText = "Recipe: \(recipe.name), Ingredients: \(recipe.ingredients), Steps: \(recipe.steps)"
+        //let tweetUrl = ""
+        let shareString = "https://twitter.com/intent/tweet?text=\(tweetText)"
+        //encode the shareString (space) for url cast
+        let escapedShareString = shareString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        //cast to an url
+        let url = URL(string: escapedShareString)
+        //content displayed on the alert
+        let alert = UIAlertController(title:"Recipe details", message: "Share this recipe", preferredStyle: .actionSheet)
+        
+        let action1 = UIAlertAction(title: "Share on Twitter", style: .default) { (action )in
+           //open the retweet in the browser
+            UIApplication.shared.open(url!)
+        }
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        //add action to action sheet
+        alert.addAction(action1)
+        alert.addAction(actionCancel)
+        //present or we say pop up the alert
+        self.present(alert, animated: true, completion:nil)
+ 
+    }
     var recipe = Recipe()
 
     override func viewDidLoad() {
