5fc5c981cbed241da83935da343466dae3109173
Tim <102419310@student.swin.edu.au>
2021-06-06 15:22:12 +1000

    add to history validation

--- MyRecipe/controller/ViewRecipeViewController.swift
+++ MyRecipe/controller/ViewRecipeViewController.swift
@@ -18,27 +18,46 @@ class ViewRecipeViewController: UIViewController {
     //save the tried recipe
     @IBAction func finish(_ sender: Any) {
         let alert = UIAlertController(title: "Rate this recipe", message: "How you went through this recipe", preferredStyle: UIAlertController.Style.alert )
-            let save = UIAlertAction(title: "Save", style: .default) { (alertAction) in
+        let save = UIAlertAction(title: "Save", style: .default) { (alertAction) in
             let rating = alert.textFields![0] as UITextField
             let comment = alert.textFields![1] as UITextField
+            
+            if comment.text == ""{
+                comment.text = "No comments"
+            }
             let formatter = DateFormatter()
-                formatter.timeStyle = .medium
+            formatter.timeStyle = .medium
             let time = formatter.string(from: Date())
             let db = DBHelper()
-                db.insertHistory(name: self.recipe.name, date: time, comment: comment.text!, rating: Int(rating.text!)!)
+            db.insertHistory(name: self.recipe.name, date: time, comment: comment.text!, rating: Int(rating.text!)!)
+        }
+        
+        //https://stackoverflow.com/questions/24037711/get-the-length-of-a-string
+        //use the notificationCenter for observer/validation
+        save.isEnabled = false
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Enter your rating of the recipe (0-9)"
+            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: OperationQueue.main) { (notification) in
+                save.isEnabled = textField.text!.count == 1 && Int(textField.text!) != nil
             }
-                alert.addTextField { (textField) in
-                textField.placeholder = "Enter your rating of the recipe (0-5) "
-                }
-                alert.addTextField { (textField) in
-                textField.placeholder = "Enter your short comments"
-                }
-            let cancel = UIAlertAction(title: "Cancel", style: .default) { (alertAction) in }
-                alert.addAction(save)
-                alert.addAction(cancel)
-                self.present(alert, animated:true, completion: nil)
-            }
-
+        })
+        /*
+         alert.addTextField { (textField) in
+         textField.placeholder = "Enter your rating of the recipe (0-5) "
+         }
+         */
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter your short comments"
+        }
+        
+        //rating as one digit number
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .default) { (alertAction) in }
+        alert.addAction(save)
+        alert.addAction(cancel)
+        self.present(alert, animated:true, completion: nil)
+    }
+    
     //share the recipe to twitter
     @IBAction func share(_ sender: Any) {
         //content of the tweet
@@ -53,7 +72,7 @@ class ViewRecipeViewController: UIViewController {
         let alert = UIAlertController(title:"Recipe details", message: "Share this recipe", preferredStyle: .actionSheet)
         
         let action1 = UIAlertAction(title: "Share on Twitter", style: .default) { (action )in
-           //open the retweet in the browser
+            //open the retweet in the browser
             UIApplication.shared.open(url!)
         }
         let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
@@ -62,16 +81,16 @@ class ViewRecipeViewController: UIViewController {
         alert.addAction(actionCancel)
         //present or we say pop up the alert
         self.present(alert, animated: true, completion:nil)
- 
+        
     }
     
     var recipe = Recipe()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         recipeName.text = recipe.name
         difficulty.text = "Difficulty: " + String(recipe.difficulty) + "🌟"
-        preparationtime.text = recipe.cooktime
+        preparationtime.text = recipe.cooktime + " mins"
         ingredients.text = recipe.ingredients
         steps.text = recipe.steps
         // Do any additional setup after loading the view.
